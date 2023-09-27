dbd2738cab7d47873fab80d0fb94335db71f5429
Lovers2020 <dkrkskswl@naver.com>
2023-09-27 13:45:19 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,17 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+
+}
+
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newTodo);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
