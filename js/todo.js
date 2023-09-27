66d28e73781357412945aac815412464793c4ece
Lovers2020 <dkrkskswl@naver.com>
2023-09-27 14:34:46 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const toDos = [];
+
+function deleteTodo(event) {
+  // console.log(event.composedPath());
+  const li = event.target.parentElement;
+  li.remove();
+
+}
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click",deleteTodo,false);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 
 }
 
@@ -12,6 +27,7 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
 }
 
