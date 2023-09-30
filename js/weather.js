1f703627822df98e0a7d45ad52086df51d9dedd8
Lovers2020 <dkrkskswl@naver.com>
2023-09-30 13:09:58 +0900

    Update file

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "5f36f1ffb61f125fd67aa51147174af7";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    weather.innerText = `Weather : ${data.weather[0].main}, Temperature : ${data.main.temp}`;
+    city.innerText = ` Location : ${data.name}`;
+  });
+}
+function onGeoError() {
+    alert("Can not find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
